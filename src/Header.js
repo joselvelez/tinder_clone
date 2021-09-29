4186446f9f43f4ff572c4e39046d659c37d27302
joselvelez <joselvelez@protonmail.com>
2021-09-29 18:01:11 -0500

    @49:00

--- src/Header.js
+++ src/Header.js
@@ -2,20 +2,24 @@ import React from 'react';
 import './Header.css';
 import PersonIcon from "@material-ui/icons/Person";
 import IconButton from '@material-ui/core/IconButton';
+import ForumIcon from '@material-ui/icons/Forum';
 
 function Header() {
     return(
         <div className='header'>
-            <h2>header</h2>
             <IconButton>
                 <PersonIcon fontSize="large" className="header__icon" />
             </IconButton>
 
             <img
                 className="header__logo"
-                src="https://logodownload.org/wp-content/uploads/2020/09/tinder-logo-0.png"
+                src="https://logos-world.net/wp-content/uploads/2020/09/Tinder-Symbol.png"
                 alt=""
             />
+
+            <IconButton>
+                <ForumIcon fontSize="large" className="header__icon" />
+            </IconButton>
         </div>
     );
 }

0f71c508829467c230b1aa07eec8fd4afdc35bd5
Jose L. Velez <48886275+joselvelez@users.noreply.github.com>
2021-09-30 08:23:51 -0500

    1:42

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import TinderCards from './TinderCards';
+import SwipeButtons from './SwipeButtons';
 
 function App() {
   return (
     <div className="app">
       <Header />
-      {/* TinderCards */}
-      {/* SwipeButtons */}
+      <TinderCards />
+      <SwipeButtons />
     </div>
   );
 }
